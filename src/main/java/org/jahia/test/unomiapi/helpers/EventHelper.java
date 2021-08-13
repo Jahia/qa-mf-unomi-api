7f288d33def370bc47fe80fa7926eff8f1837079
ksagnard <ksagnard@jahia.com>
2021-08-13 14:44:44 +0200

    take comments into account - partial

--- src/main/java/org/jahia/test/unomiapi/helpers/EventHelper.java
+++ src/main/java/org/jahia/test/unomiapi/helpers/EventHelper.java
@@ -1,18 +1,16 @@
 package org.jahia.test.unomiapi.helpers;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
 import com.mashape.unirest.http.HttpMethod;
 import io.restassured.specification.RequestSpecification;
-import org.jahia.test.unomiapi.data.Event.Condition;
-import org.jahia.test.unomiapi.data.Event.EventListRequest;
 import org.jahia.test.unomiapi.data.TestGlobalConfiguration;
 import org.jahia.test.unomiapi.data.UnomiApiScenarioRuntimeData;
-import org.jahia.test.unomiapi.data.Event.ParameterValues;
+import org.jahia.selenium;
+import org.json.JSONObject;
 
 import java.net.URL;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
+import java.util.Arrays;
 
 public class EventHelper {
 
@@ -30,12 +28,19 @@ public class EventHelper {
         Long minTime = now.minus(1, ChronoUnit.DAYS).toEpochMilli();
         Long maxTime = now.plus(1, ChronoUnit.DAYS).toEpochMilli();
 
-        ParameterValues parameterValues = new ParameterValues("between", "timeStamp", minTime, maxTime);
-        Condition condition = new Condition("sessionPropertyCondition", parameterValues);
-        EventListRequest eventRequest = new EventListRequest(condition);
+        JSONObject conditionParameters = new JSONObject();
+        conditionParameters.put("propertyName", "timeStamp");
+        conditionParameters.put("comparisonOperator", "between");
+        conditionParameters.put("propertyValues", Arrays.asList(minTime, maxTime));
 
-        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
-        String requestBodyJson = ow.writeValueAsString(eventRequest);
+        JSONObject condition = new JSONObject();
+        condition.put("type", "sessionPropertyCondition");
+        condition.put("parameterValues", conditionParameters);
+
+        JSONObject query = new JSONObject();
+        query.put("condition", condition);
+
+        String requestBodyJson = query.toString();
 
         RestRequestHelper reqHelper = new RestRequestHelper(unomiApiScenarioRuntimeData);
         unomiApiScenarioRuntimeData.setResponse(reqHelper.sendRequest(req,
@@ -44,7 +49,18 @@ public class EventHelper {
 
         String response = this.unomiApiScenarioRuntimeData.getResponse().asString();
 
+
+
         // TODO : read response as object
+        // JSONObject responseJson = new JSONObject(response);
+        // Here we have only a partialPath (ex: "JahiaMfIntegTests/home/perso-on-goal-page.html" instead of
+        // "http://localhost:8080/sites/JahiaMfIntegTests/home/perso-on-goal-page.html")
+        // A simple get("http://localhost:8080/sites/JahiaMfIntegTests/home/perso-on-goal-page.html") does not work
+        // A solution i was trying was to pass JahiaSeleniumConfiguration.getPropertyValue("Dselenium.jahia.host","") but JahiaSeleniumConfiguration
+        // is not recognized
+        // impacted feature to test : mostVisitedPage.feature
+        // branch for the qa-mf-selenium side of this task  https://github.com/Jahia/qa-mf-selenium/tree/DMF-4868_RemoveUITestMostVisitedPages
+
         if(response.indexOf(partialPagePath) <0 ) {return 0; }
         else {
             String test = response.split(partialPagePath + "\":")[1];
