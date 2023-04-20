6c922195b9f7bf23497c5ab87b08835749a49746
ksagnard <ksagnard@jahia.com>
2023-04-20 10:38:37 +0200

    Revert "SELENIUM-1587 : fix after adding flattened properties (#19)" (#20)
    This reverts commit b480c0d6f8ab3d6fb9acad6458fa90e63029b4ed.

--- src/main/java/org/jahia/test/unomiapi/helpers/EventHelper.java
+++ src/main/java/org/jahia/test/unomiapi/helpers/EventHelper.java
@@ -203,7 +203,7 @@ public class EventHelper {
 
         RestRequestHelper reqHelper = new RestRequestHelper(unomiApiScenarioRuntimeData);
         unomiApiScenarioRuntimeData.setResponse(reqHelper.sendRequest(req,
-                new URL(TestGlobalConfiguration.getUnomiUrl() + "/cxs/query/event/source.flattenedProperties.fields.src_terms%5B0%5D.term"),
+                new URL(TestGlobalConfiguration.getUnomiUrl() + "/cxs/query/event/properties.keyword"),
                 requestBodyJson, HttpMethod.POST));
 
         String response = this.unomiApiScenarioRuntimeData.getResponse().asString();
